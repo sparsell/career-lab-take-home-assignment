129741ce27dcc54d6b5f796aa9eae4ae538c69f4
sparsell <hello@sparsell.com>
2022-06-07 13:07:33 -0400

    added prevent default to search form

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -6,6 +6,7 @@ export function SearchForm({ onSearchSubmit }) {
 	const [query, setQuery] = useState('');
 
 	function handleInputChange(evt) {
+		evt.preventDefault();
 		setQuery(evt.target.value);
 	}
 
